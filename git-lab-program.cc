059a72281d0b3ff453cb9efc432268af51ae00ba
McKenzie Searcy <ms608122@ohio.edu>
2024-05-17 21:24:58 -0400

    done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: McKenzie Searcy
+ *        Date: 05-17-24
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
